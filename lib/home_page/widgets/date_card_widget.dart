207b29c643cfbf0db95adb7a4721eaa59ac3fdf7
BillyOroh <bleezxz@gmail.com>
2023-11-18 17:00:25 +0800

    update 13

--- lib/home_page/widgets/date_card_widget.dart
+++ lib/home_page/widgets/date_card_widget.dart
@@ -12,12 +12,15 @@ class DateCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(left: 25),
       padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
       decoration: BoxDecoration(
         color: Colors.grey.shade100,
         borderRadius: BorderRadius.circular(20.0),
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text(
             dateModel.day,
